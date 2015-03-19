a1f549f3ea3aedd626d2a4a2ae01f4f75fa4353f
Dillon Huff <dillonhuff@gmail.com>
2015-03-19 15:26:10 -0500

    refactored the Lang java grammar wrapper conversion code into Parser.hs

--- src/Javalyzer/Parser.hs
+++ src/Javalyzer/Parser.hs
@@ -2,6 +2,7 @@ module Javalyzer.Parser(parseCompilationUnit) where
 
 import Data.List as L
 import Language.Java.Parser
+import Language.Java.Syntax
 import Text.Parsec.Error
 
 import Javalyzer.Syntax
@@ -11,3 +12,266 @@ parseCompilationUnit :: String -> JError JCompilationUnit
 parseCompilationUnit name = case parser compilationUnit name of
   Left err -> fail $ show err
   Right compUnit -> compUnitToJ compUnit
+
+compUnitToJ :: CompilationUnit -> JError JCompilationUnit
+compUnitToJ (CompilationUnit packDecl imports typeDecls) = do
+  packDeclJ <- maybePackageDeclToJ packDecl
+  importsJ <- mapM importToJ imports
+  typeDeclsJ <- mapM typeDeclToJ typeDecls
+  return $ jCompUnit packDeclJ importsJ typeDeclsJ
+
+maybePackageDeclToJ :: Maybe PackageDecl -> JError (Maybe JPackageDecl)
+maybePackageDeclToJ Nothing = return Nothing
+maybePackageDeclToJ (Just dec) =
+  let pkg = packageDeclToJ dec in
+  case pkg of
+    (JSuccess p) -> JSuccess (Just p)
+    (JFail str) -> (JFail str)
+
+  
+packageDeclToJ :: PackageDecl -> JError JPackageDecl
+packageDeclToJ (PackageDecl n) = do
+  nJ <- nameToJ n
+  return $ jPackageDecl nJ
+
+importToJ :: ImportDecl -> JError JImportDecl
+importToJ im = return jId
+
+typeDeclToJ :: TypeDecl -> JError JTypeDecl
+typeDeclToJ (ClassTypeDecl (ClassDecl mods id tps sup refs body)) = do
+  modsJ <- mapM modifierToJ mods
+  idJ <- identToJ id
+  tpsJ <- mapM typeParamToJ tps
+  supJ <- superToJ sup
+  refsJ <- mapM refTypeToJ refs
+  bodyJ <- classBodyToJ body
+  return $ jClassTypeDecl modsJ idJ tpsJ supJ refsJ bodyJ
+typeDeclToJ other = fail $ (show other) ++ " is not supported by typeDeclToJ"
+
+classBodyToJ (ClassBody decls) = do
+  declsJ <- mapM declToJ decls
+  return $ jClassBody declsJ
+
+declToJ (MemberDecl (MethodDecl mods tps retType id fparams exceptions body)) = do
+  modsJ <- mapM modifierToJ mods
+  tpsJ <- mapM typeParamToJ tps
+  retTypeJ <- returnTypeToJ retType
+  idJ <- identToJ id
+  fparamsJ <- mapM formalParamToJ fparams
+  exceptionsJ <- mapM exceptionTypeToJ exceptions
+  bodyJ <- methodBodyToJ body
+  return $ jMemberDecl $ jMethodDecl modsJ tpsJ retTypeJ idJ fparamsJ exceptionsJ bodyJ
+declToJ (MemberDecl (FieldDecl mods tp varDecls)) = do
+  modsJ <- mapM modifierToJ mods
+  tpJ <- typeToJ tp
+  varDeclsJ <- mapM varDeclToJ varDecls
+  return $ jMemberDecl $ jFieldDecl modsJ tpJ varDeclsJ
+declToJ (MemberDecl (ConstructorDecl mods tps id fps exps body)) = do
+  modsJ <- mapM modifierToJ mods
+  tpsJ <- mapM typeParamToJ tps
+  idJ <- identToJ id
+  fpsJ <- mapM formalParamToJ fps
+  expsJ <- mapM exceptionTypeToJ exps
+  bodJ <- constructorBodyToJ body
+  return $ jMemberDecl $ jConstructorDecl modsJ tpsJ idJ fpsJ expsJ bodJ
+declToJ other = fail $ (show other) ++ " is not suported by declToJ"
+
+constructorBodyToJ (ConstructorBody Nothing stmts) = do
+  stmtsJ <- mapM blockStmtToJ stmts
+  return $ jConstructorBody Nothing stmtsJ
+constructorBodyToJ (ConstructorBody (Just explCall) stmts) = do
+  explCallJ <- explConstrInvToJ explCall
+  stmtsJ <- mapM blockStmtToJ stmts
+  return $ jConstructorBody (Just explCallJ) stmtsJ
+
+explConstrInvToJ (SuperInvoke refs args) = do
+  refsJ <- mapM refTypeToJ refs
+  argsJ <- mapM expToJ args
+  return $ jSuperInvoke refsJ argsJ
+explConstrInvToJ other = fail $ (show other) ++ " is not supported by explConstrInvToJ"
+
+methodBodyToJ :: MethodBody -> JError JMethodBody
+methodBodyToJ (MethodBody Nothing) = return $ jMethodBody Nothing
+methodBodyToJ (MethodBody (Just (Block stmts))) = do
+  stmtsJ <- mapM blockStmtToJ stmts
+  return $ jBlockMethod $ jBlock stmtsJ
+
+blockStmtToJ :: BlockStmt -> JError JBlockStmt
+blockStmtToJ (BlockStmt stmt) = do
+  stmtJ <- stmtToJ stmt
+  return $ jBlockStmt stmtJ
+blockStmtToJ (LocalVars mods tp decls) = do
+  modsJ <- mapM modifierToJ mods
+  tpJ <- typeToJ tp
+  declsJ <- mapM varDeclToJ decls
+  return $ jLocalVars modsJ tpJ declsJ
+blockStmtToJ other = fail $ (show other) ++ " is not supported by blockStmtToJ"
+
+stmtToJ :: Stmt -> JError JStmt
+stmtToJ (Return Nothing) = return jReturnVoid
+stmtToJ (Return (Just ex)) = do
+  exJ <- expToJ ex
+  return $ jReturn exJ
+stmtToJ (ExpStmt exp) = do
+  expJ <- expToJ exp
+  return $ jExpStmt expJ
+stmtToJ other = fail $ (show other) ++ " is not supported by stmtToJ"
+
+expToJ (Lit l) = do
+  lJ <- literalToJ l
+  return $ jLit lJ
+expToJ (Assign lhs asgOp exp) = do
+  lhsJ <- lhsToJ lhs
+  asgOpJ <- assignOpToJ asgOp
+  expJ <- expToJ exp
+  return $ jAssign lhsJ asgOpJ expJ
+expToJ (MethodInv inv) = do
+  invJ <- methodInvocationToJ inv
+  return $ jMethodInv invJ
+expToJ (ExpName n) = do
+  nJ <- nameToJ n
+  return $ jExpName nJ
+expToJ (FieldAccess fa) = do
+  faJ <- fieldAccessToJ fa
+  return $ jFieldAccess faJ
+expToJ This = return jThis
+expToJ other = fail $ (show other) ++ " is not supported by expToJ"
+
+fieldAccessToJ (PrimaryFieldAccess exp id) = do
+  expJ <- expToJ exp
+  idJ <- identToJ id
+  return $ jPrimaryFieldAccess expJ idJ
+fieldAccessToJ other = fail $ (show other) ++ " is not supported by fieldAccessToJ"
+
+methodInvocationToJ (PrimaryMethodCall exp refTs id args) = do
+  expJ <- expToJ exp
+  refTsJ <- mapM refTypeToJ refTs
+  idJ <- identToJ id
+  argsJ <- mapM expToJ args
+  return $ jPrimaryMethodCall expJ refTsJ idJ argsJ
+methodInvocationToJ (MethodCall n args) = do
+  nJ <- nameToJ n
+  argsJ <- mapM expToJ args
+  return $ jMethodCall nJ argsJ
+methodInvocationToJ other = fail $ (show other) ++ " is not supported by methodInvocationToJ"
+
+literalToJ Null = return jNull
+literalToJ (Int i) = return $ jInt i
+literalToJ (Boolean b) = return $ jBoolean b
+literalToJ (Char c) = return $ jChar c
+literalToJ (Float f) = return $ jFloat f
+literalToJ (Double d) = return $ jDouble d
+literalToJ (String s) = return $ jString s
+literalToJ other = fail $ (show other) ++ " is not supported by literalToJ"
+
+varDeclToJ (VarDecl vid Nothing) = do
+  vidJ <- varDeclIdToJ vid
+  return $ jVarDecl vidJ Nothing
+varDeclToJ (VarDecl vid (Just vinit)) = do
+  vidJ <- varDeclIdToJ vid
+  vinitJ <- varInitToJ vinit
+  return $ jVarDecl vidJ (Just vinitJ)
+
+varDeclIdToJ (VarId id) = do
+  idJ <- identToJ id
+  return $ jVarId idJ
+varDeclIdToJ other = fail $ (show other) ++ " is not supported by varDeclIdToJ"
+
+varInitToJ (InitExp ex) = do
+  exJ <- expToJ ex
+  return $ jInitExp exJ
+
+modifierToJ :: Modifier -> JError JModifier
+modifierToJ Public = return jPublic
+modifierToJ Private = return jPrivate
+modifierToJ Protected = return jProtected
+modifierToJ Final = return jFinal
+modifierToJ Abstract = return jAbstract
+modifierToJ Static = return jStatic
+modifierToJ (Annotation ann) = do
+  annJ <- annotationToJ ann
+  return $ jAnnotation annJ
+modifierToJ m = fail $ show m ++ " is not a supported modifier"
+
+lhsToJ (NameLhs id) = do
+  idJ <- nameToJ id
+  return $ jNameLhs idJ
+lhsToJ (FieldLhs fa) = do
+  faJ <- fieldAccessToJ fa
+  return $ jFieldLhs faJ 
+lhsToJ other = fail $ (show other) ++ " is not supported by lhsToJ"
+
+nameToJ (Name ids) = do
+  idsJ <- mapM identToJ ids
+  return $ jName idsJ
+
+typeToJ (RefType rt) = do
+  rtJ <- refTypeToJ rt
+  return $ jRefType rtJ
+typeToJ (PrimType pt) = do
+  ptJ <- primTypeToJ pt
+  return $ jPrimType ptJ
+
+returnTypeToJ Nothing = return $ Nothing
+returnTypeToJ (Just tp) = do
+  tpJ <- typeToJ tp
+  return $ Just tpJ
+
+primTypeToJ BooleanT = return jBooleanT
+primTypeToJ ByteT = return jByteT
+primTypeToJ ShortT = return jShortT
+primTypeToJ IntT = return jIntT
+primTypeToJ LongT = return jLongT
+primTypeToJ CharT = return jCharT
+primTypeToJ FloatT = return jFloatT
+primTypeToJ DoubleT = return jDoubleT
+
+typeParamToJ (TypeParam id refs) = do
+  idJ <- identToJ id
+  refsJ <- mapM refTypeToJ refs
+  return $ jTypeParam idJ refsJ
+
+formalParamToJ :: FormalParam -> JError JFormalParam
+formalParamToJ (FormalParam mods tp False varDec) = do
+  modsJ <- mapM modifierToJ mods
+  tpJ <- typeToJ tp
+  varDecJ <- varDeclIdToJ varDec
+  return $ jFormalParam modsJ tpJ False varDecJ
+formalParamToJ other = fail $ (show other) ++ " is not supported by formalParamToJ"
+
+refTypeToJ :: RefType -> JError JRefType
+refTypeToJ (ClassRefType (ClassType idTypeArgList)) = do
+  idTypeArgListJ <- idTypeArgListToJ idTypeArgList
+  return $ jClassRefType $ jClassType idTypeArgListJ
+refTypeToJ other = fail $ "refTypeToJ not implemented for " ++ (show other)
+
+idTypeArgListToJ :: [(Ident, [TypeArgument])] -> JError [(JIdent, [JTypeArgument])]
+idTypeArgListToJ ls = 
+  let ids = L.map fst ls
+      tArgs = L.map snd ls in
+  do
+    idsJ <- mapM identToJ ids
+    tArgsJ <- mapM (\t -> mapM typeArgumentToJ t) tArgs
+    return $ L.zip idsJ tArgsJ
+
+superToJ :: Maybe RefType -> JError (Maybe JRefType)
+superToJ (Just ref) = do
+  refJ <- refTypeToJ ref
+  return $ Just refJ
+superToJ Nothing = return Nothing
+
+exceptionTypeToJ :: ExceptionType -> JError JExceptionType
+exceptionTypeToJ ex = refTypeToJ ex
+
+annotationToJ (MarkerAnnotation n) = do
+  nJ <- nameToJ n
+  return $ jMarkerAnnotation nJ
+annotationToJ other = fail $ (show other) ++ " is not supported by annotationToJ"
+
+assignOpToJ EqualA = return jEqualA
+assignOpToJ other = fail $ (show other) ++ " is not supported by assignOpToJ"
+
+identToJ :: Ident -> JError JIdent
+identToJ (Ident n) = return $ jIdent n
+
+typeArgumentToJ ta = fail "typeArgumentToJ not implemented"
--- src/Javalyzer/Syntax.hs
+++ src/Javalyzer/Syntax.hs
@@ -1,6 +1,5 @@
 module Javalyzer.Syntax(JParseError,
                         JCompilationUnit,
-                        compUnitToJ,                        
                         jCompUnit,
                         package,
                         imports,
@@ -9,6 +8,7 @@ module Javalyzer.Syntax(JParseError,
                         isSingleClass,
                         JTypeDecl,
                         JClassDecl,
+                        JMethodBody,
                         jClassDecl,
                         classBody,
                         className,
@@ -54,6 +54,7 @@ module Javalyzer.Syntax(JParseError,
                         jExpName,
                         jEqualA,
                         JType,
+                        JRefType,
                         jRefType,
                         JVarDecl,
                         JVarDeclIdType(..),
@@ -97,7 +98,15 @@ module Javalyzer.Syntax(JParseError,
                         jProtected,
                         jAnnotation,
                         jParseError,
-                        jMarkerAnnotation) where
+                        jMarkerAnnotation,
+                        JTypeArgument,
+                        JExceptionType,
+                        JFormalParam,
+                        JStmt,
+                        JAnnotation,
+                        jId,
+                        JBlockStmt,
+                        jMethodBody) where
 
 import Control.Monad
 import Data.List as L
@@ -127,36 +136,16 @@ isSingleClass (JCompilationUnit _ _ decls) =
       _ -> False
     False -> False
 
-compUnitToJ :: CompilationUnit -> JError JCompilationUnit
-compUnitToJ (CompilationUnit packDecl imports typeDecls) = do
-  packDeclJ <- maybePackageDeclToJ packDecl
-  importsJ <- mapM importToJ imports
-  typeDeclsJ <- mapM typeDeclToJ typeDecls
-  return $ jCompUnit packDeclJ importsJ typeDeclsJ
 
 data JPackageDecl = JPackageDecl JName
                     deriving (Eq, Ord, Show)
 
 jPackageDecl = JPackageDecl
 
-maybePackageDeclToJ :: Maybe PackageDecl -> JError (Maybe JPackageDecl)
-maybePackageDeclToJ Nothing = return Nothing
-maybePackageDeclToJ (Just dec) =
-  let pkg = packageDeclToJ dec in
-  case pkg of
-    (JSuccess p) -> JSuccess (Just p)
-    (JFail str) -> (JFail str)
-  
-packageDeclToJ :: PackageDecl -> JError JPackageDecl
-packageDeclToJ (PackageDecl n) = do
-  nJ <- nameToJ n
-  return $ jPackageDecl nJ
-
 data JImportDecl = JID
                    deriving (Eq, Ord, Show)
 
-importToJ :: ImportDecl -> JError JImportDecl
-importToJ im = return JID
+jId = JID
 
 data JTypeDecl = JClassTypeDecl JClassDecl
                  deriving (Eq, Ord, Show)
@@ -167,17 +156,6 @@ jClassTypeDecl mods name typeParams super refTypes body =
 isClassDecl (JClassTypeDecl _) = True
 
 getClassDecl (JClassTypeDecl d) = d
-
-typeDeclToJ :: TypeDecl -> JError JTypeDecl
-typeDeclToJ (ClassTypeDecl (ClassDecl mods id tps sup refs body)) = do
-  modsJ <- mapM modifierToJ mods
-  idJ <- identToJ id
-  tpsJ <- mapM typeParamToJ tps
-  supJ <- superToJ sup
-  refsJ <- mapM refTypeToJ refs
-  bodyJ <- classBodyToJ body
-  return $ jClassTypeDecl modsJ idJ tpsJ supJ refsJ bodyJ
-typeDeclToJ other = fail $ (show other) ++ " is not supported by typeDeclToJ"
 
 data JClassDecl = JClassDecl [JModifier] JIdent [JTypeParam] (Maybe JRefType) [JRefType] JClassBody
                   deriving (Eq, Ord, Show)
@@ -193,10 +171,6 @@ jClassBody = JClassBody
 
 bodyDecls (JClassBody ds) = ds
 
-classBodyToJ (ClassBody decls) = do
-  declsJ <- mapM declToJ decls
-  return $ jClassBody declsJ
-
 data JDecl = JMemberDecl JMemberDecl
              deriving (Eq, Ord, Show)
 
@@ -210,30 +184,6 @@ declType (JMemberDecl _) = MEMBER
 
 jMemberDecl = JMemberDecl
 
-declToJ (MemberDecl (MethodDecl mods tps retType id fparams exceptions body)) = do
-  modsJ <- mapM modifierToJ mods
-  tpsJ <- mapM typeParamToJ tps
-  retTypeJ <- returnTypeToJ retType
-  idJ <- identToJ id
-  fparamsJ <- mapM formalParamToJ fparams
-  exceptionsJ <- mapM exceptionTypeToJ exceptions
-  bodyJ <- methodBodyToJ body
-  return $ jMemberDecl $ jMethodDecl modsJ tpsJ retTypeJ idJ fparamsJ exceptionsJ bodyJ
-declToJ (MemberDecl (FieldDecl mods tp varDecls)) = do
-  modsJ <- mapM modifierToJ mods
-  tpJ <- typeToJ tp
-  varDeclsJ <- mapM varDeclToJ varDecls
-  return $ jMemberDecl $ jFieldDecl modsJ tpJ varDeclsJ
-declToJ (MemberDecl (ConstructorDecl mods tps id fps exps body)) = do
-  modsJ <- mapM modifierToJ mods
-  tpsJ <- mapM typeParamToJ tps
-  idJ <- identToJ id
-  fpsJ <- mapM formalParamToJ fps
-  expsJ <- mapM exceptionTypeToJ exps
-  bodJ <- constructorBodyToJ body
-  return $ jMemberDecl $ jConstructorDecl modsJ tpsJ idJ fpsJ expsJ bodJ
-                       
-declToJ other = fail $ (show other) ++ " is not suported by declToJ"
 
 data JMemberDecl
   = JMethodDecl [JModifier] [JTypeParam] (Maybe JType) JIdent [JFormalParam] [JExceptionType] JMethodBody
@@ -264,37 +214,17 @@ data JConstructorBody = JConstructorBody (Maybe JExplConstrInv) [JBlockStmt]
 
 jConstructorBody = JConstructorBody
 
-constructorBodyToJ (ConstructorBody Nothing stmts) = do
-  stmtsJ <- mapM blockStmtToJ stmts
-  return $ jConstructorBody Nothing stmtsJ
-constructorBodyToJ (ConstructorBody (Just explCall) stmts) = do
-  explCallJ <- explConstrInvToJ explCall
-  stmtsJ <- mapM blockStmtToJ stmts
-  return $ jConstructorBody (Just explCallJ) stmtsJ
---constructorBodyToJ other = fail $ (show other) ++ " is not supported by constructorBodyToJ"
-
 data JExplConstrInv
   = JSuperInvoke [JRefType] [JArgument]
     deriving (Eq, Ord, Show)
 
 jSuperInvoke = JSuperInvoke
 
-explConstrInvToJ (SuperInvoke refs args) = do
-  refsJ <- mapM refTypeToJ refs
-  argsJ <- mapM expToJ args
-  return $ jSuperInvoke refsJ argsJ
-explConstrInvToJ other = fail $ (show other) ++ " is not supported by explConstrInvToJ"
-
 data JMethodBody = JMethodBody (Maybe JBlock)
                    deriving (Eq, Ord, Show)
 
+jMethodBody m = JMethodBody m
 jBlockMethod block = JMethodBody $ Just block
-
-methodBodyToJ :: MethodBody -> JError JMethodBody
-methodBodyToJ (MethodBody Nothing) = return $ JMethodBody Nothing
-methodBodyToJ (MethodBody (Just (Block stmts))) = do
-  stmtsJ <- mapM blockStmtToJ stmts
-  return $ jBlockMethod $ jBlock stmtsJ
 
 data JBlock = JBlock [JBlockStmt]
               deriving (Eq, Ord, Show)
@@ -309,17 +239,6 @@ data JBlockStmt
 jBlockStmt = JBlockStmt
 jLocalVars = JLocalVars
 
-blockStmtToJ :: BlockStmt -> JError JBlockStmt
-blockStmtToJ (BlockStmt stmt) = do
-  stmtJ <- stmtToJ stmt
-  return $ JBlockStmt stmtJ
-blockStmtToJ (LocalVars mods tp decls) = do
-  modsJ <- mapM modifierToJ mods
-  tpJ <- typeToJ tp
-  declsJ <- mapM varDeclToJ decls
-  return $ jLocalVars modsJ tpJ declsJ
-blockStmtToJ other = fail $ (show other) ++ " is not supported by blockStmtToJ"
-
 data JStmt
   = JReturn (Maybe JExp)
   | JExpStmt JExp
@@ -329,16 +248,6 @@ jReturnVoid = JReturn Nothing
 jReturn exp = JReturn $ Just exp
 
 jExpStmt = JExpStmt
-
-stmtToJ :: Stmt -> JError JStmt
-stmtToJ (Return Nothing) = return jReturnVoid
-stmtToJ (Return (Just ex)) = do
-  exJ <- expToJ ex
-  return $ jReturn exJ
-stmtToJ (ExpStmt exp) = do
-  expJ <- expToJ exp
-  return $ jExpStmt expJ
-stmtToJ other = fail $ (show other) ++ " is not supported by stmtToJ"
 
 data JExp
   = JLit JLiteral
@@ -356,26 +265,6 @@ jExpName = JExpName
 jFieldAccess = JFieldAccess
 jThis = JThis
 
-expToJ (Lit l) = do
-  lJ <- literalToJ l
-  return $ jLit lJ
-expToJ (Assign lhs asgOp exp) = do
-  lhsJ <- lhsToJ lhs
-  asgOpJ <- assignOpToJ asgOp
-  expJ <- expToJ exp
-  return $ jAssign lhsJ asgOpJ expJ
-expToJ (MethodInv inv) = do
-  invJ <- methodInvocationToJ inv
-  return $ jMethodInv invJ
-expToJ (ExpName n) = do
-  nJ <- nameToJ n
-  return $ jExpName nJ
-expToJ (FieldAccess fa) = do
-  faJ <- fieldAccessToJ fa
-  return $ jFieldAccess faJ
-expToJ This = return JThis
-expToJ other = fail $ (show other) ++ " is not supported by expToJ"
-
 type JArgument = JExp
 
 data JFieldAccess
@@ -384,12 +273,6 @@ data JFieldAccess
 
 jPrimaryFieldAccess = JPrimaryFieldAccess
 
-fieldAccessToJ (PrimaryFieldAccess exp id) = do
-  expJ <- expToJ exp
-  idJ <- identToJ id
-  return $ jPrimaryFieldAccess expJ idJ
-fieldAccessToJ other = fail $ (show other) ++ " is not supported by fieldAccessToJ"
-
 data JMethodInvocation
   = JPrimaryMethodCall JExp [JRefType] JIdent [JArgument]
   | JMethodCall JName [JArgument]
@@ -397,18 +280,6 @@ data JMethodInvocation
 
 jPrimaryMethodCall = JPrimaryMethodCall
 jMethodCall = JMethodCall
-
-methodInvocationToJ (PrimaryMethodCall exp refTs id args) = do
-  expJ <- expToJ exp
-  refTsJ <- mapM refTypeToJ refTs
-  idJ <- identToJ id
-  argsJ <- mapM expToJ args
-  return $ jPrimaryMethodCall expJ refTsJ idJ argsJ
-methodInvocationToJ (MethodCall n args) = do
-  nJ <- nameToJ n
-  argsJ <- mapM expToJ args
-  return $ jMethodCall nJ argsJ
-methodInvocationToJ other = fail $ (show other) ++ " is not supported by methodInvocationToJ"
 
 data JLiteral
   = JNull
@@ -430,23 +301,11 @@ jFloat = JFloat
 jDouble = JDouble
 jString = JString
 
-literalToJ Null = return jNull
-literalToJ (Int i) = return $ JInt i
-literalToJ (Boolean b) = return $ JBoolean b
-literalToJ (Char c) = return $ JChar c
-literalToJ (Float f) = return $ JFloat f
-literalToJ (Double d) = return $ JDouble d
-literalToJ (String s) = return $ JString s
-literalToJ other = fail $ (show other) ++ " is not supported by literalToJ"
-
 data JAssignOp
   = JEqualA
     deriving (Eq, Ord, Show)
 
 jEqualA = JEqualA
-
-assignOpToJ EqualA = return jEqualA
-assignOpToJ other = fail $ (show other) ++ " is not supported by assignOpToJ"
 
 data JVarDecl = JVarDecl JVarDeclId (Maybe JVarInit)
                 deriving (Eq, Ord, Show)
@@ -454,14 +313,6 @@ data JVarDecl = JVarDecl JVarDeclId (Maybe JVarInit)
 jVarDecl = JVarDecl
 
 getVarDeclId (JVarDecl vid _) = vid
-
-varDeclToJ (VarDecl vid Nothing) = do
-  vidJ <- varDeclIdToJ vid
-  return $ jVarDecl vidJ Nothing
-varDeclToJ (VarDecl vid (Just vinit)) = do
-  vidJ <- varDeclIdToJ vid
-  vinitJ <- varInitToJ vinit
-  return $ jVarDecl vidJ (Just vinitJ)
 
 data JVarDeclId
   = JVarId JIdent
@@ -477,20 +328,11 @@ getVarIdent (JVarId id) = id
 
 jVarId = JVarId
 
-varDeclIdToJ (VarId id) = do
-  idJ <- identToJ id
-  return $ jVarId idJ
-varDeclIdToJ other = fail $ (show other) ++ " is not supported by varDeclIdToJ"
-
 data JVarInit
   = JInitExp JExp
     deriving (Eq, Ord, Show)
 
 jInitExp = JInitExp
-
-varInitToJ (InitExp ex) = do
-  exJ <- expToJ ex
-  return $ jInitExp exJ
 
 data JModifier
   = JPublic
@@ -510,18 +352,6 @@ jAbstract = JAbstract
 jStatic = JStatic
 jAnnotation = JAnnotation
 
-modifierToJ :: Modifier -> JError JModifier
-modifierToJ Public = return jPublic
-modifierToJ Private = return jPrivate
-modifierToJ Protected = return jProtected
-modifierToJ Final = return jFinal
-modifierToJ Abstract = return jAbstract
-modifierToJ Static = return jStatic
-modifierToJ (Annotation ann) = do
-  annJ <- annotationToJ ann
-  return $ JAnnotation annJ
-modifierToJ m = fail $ show m ++ " is not a supported modifier"
-
 data JLhs
   = JNameLhs JName
   | JFieldLhs JFieldAccess
@@ -530,14 +360,6 @@ data JLhs
 jNameLhs = JNameLhs
 jFieldLhs = JFieldLhs
 
-lhsToJ (NameLhs id) = do
-  idJ <- nameToJ id
-  return $ jNameLhs idJ
-lhsToJ (FieldLhs fa) = do
-  faJ <- fieldAccessToJ fa
-  return $ jFieldLhs faJ 
-lhsToJ other = fail $ (show other) ++ " is not supported by lhsToJ"
-
 data JIdent = JIdent String
               deriving (Eq, Ord, Show)
 
@@ -545,18 +367,10 @@ jIdent = JIdent
 
 jIdentName (JIdent str) = str
 
-identToJ :: Ident -> JError JIdent
-identToJ (Ident n) = return $ jIdent n
-
 data JName = JName [JIdent]
              deriving (Eq, Ord, Show)
 
 jName = JName
-
-nameToJ (Name ids) = do
-  idsJ <- mapM identToJ ids
-  return $ jName idsJ
---nameToJ other = fail $ (show other) ++ " is not supported by nameToJ"
 
 data JType
   = JPrimType JPrimType
@@ -565,18 +379,6 @@ data JType
 
 jRefType = JRefType
 jPrimType = JPrimType
-
-typeToJ (RefType rt) = do
-  rtJ <- refTypeToJ rt
-  return $ jRefType rtJ
-typeToJ (PrimType pt) = do
-  ptJ <- primTypeToJ pt
-  return $ jPrimType ptJ
-
-returnTypeToJ Nothing = return $ Nothing
-returnTypeToJ (Just tp) = do
-  tpJ <- typeToJ tp
-  return $ Just tpJ
 
 data JPrimType
   = JBooleanT
@@ -598,64 +400,20 @@ jCharT = JCharT
 jFloatT = JFloatT
 jDoubleT = JDoubleT
 
-primTypeToJ BooleanT = return jBooleanT
-primTypeToJ ByteT = return jByteT
-primTypeToJ ShortT = return jShortT
-primTypeToJ IntT = return jIntT
-primTypeToJ LongT = return jLongT
-primTypeToJ CharT = return jCharT
-primTypeToJ FloatT = return jFloatT
-primTypeToJ DoubleT = return jDoubleT
-
 data JTypeParam = JTypeParam JIdent [JRefType]
                   deriving (Eq, Ord, Show)
 
 jTypeParam = JTypeParam
-
-typeParamToJ (TypeParam id refs) = do
-  idJ <- identToJ id
-  refsJ <- mapM refTypeToJ refs
-  return $ jTypeParam idJ refsJ
 
 data JFormalParam = JFormalParam [JModifier] JType Bool JVarDeclId
                     deriving (Eq, Ord, Show)
 
 jFormalParam = JFormalParam
 
-formalParamToJ :: FormalParam -> JError JFormalParam
-formalParamToJ (FormalParam mods tp False varDec) = do
-  modsJ <- mapM modifierToJ mods
-  tpJ <- typeToJ tp
-  varDecJ <- varDeclIdToJ varDec
-  return $ JFormalParam modsJ tpJ False varDecJ
-formalParamToJ other = fail $ (show other) ++ " is not supported by formalParamToJ"
-
 data JRefType = JClassRefType JClassType
                 deriving (Eq, Ord, Show)
 
 jClassRefType = JClassRefType
-
-refTypeToJ :: RefType -> JError JRefType
-refTypeToJ (ClassRefType (ClassType idTypeArgList)) = do
-  idTypeArgListJ <- idTypeArgListToJ idTypeArgList
-  return $ jClassRefType $ jClassType idTypeArgListJ
-refTypeToJ other = fail $ "refTypeToJ not implemented for " ++ (show other)
-
-idTypeArgListToJ :: [(Ident, [TypeArgument])] -> JError [(JIdent, [JTypeArgument])]
-idTypeArgListToJ ls = 
-  let ids = L.map fst ls
-      tArgs = L.map snd ls in
-  do
-    idsJ <- mapM identToJ ids
-    tArgsJ <- mapM (\t -> mapM typeArgumentToJ t) tArgs
-    return $ L.zip idsJ tArgsJ
---  L.map (\(id, tArgs) -> (identToJ id, L.map typeArgumentToJ tArgs)) ls
-
-superToJ :: Maybe RefType -> JError (Maybe JRefType)
-superToJ (Just ref) = do
-  refJ <- refTypeToJ ref
-  return $ Just refJ
-superToJ Nothing = return Nothing
 
 data JClassType = JClassType [(JIdent, [JTypeArgument])]
                   deriving (Eq, Ord, Show)
@@ -665,20 +423,10 @@ jClassType = JClassType
 data JTypeArgument = JActualType
                      deriving (Eq, Ord, Show)
 
-typeArgumentToJ ta = fail "typeArgumentToJ not implemented"
-
 type JExceptionType = JRefType
-
-exceptionTypeToJ :: ExceptionType -> JError JExceptionType
-exceptionTypeToJ ex = refTypeToJ ex
 
 data JAnnotation
   = JMarkerAnnotation JName
     deriving (Eq, Ord, Show)
 
 jMarkerAnnotation = JMarkerAnnotation
-
-annotationToJ (MarkerAnnotation n) = do
-  nJ <- nameToJ n
-  return $ jMarkerAnnotation nJ
-annotationToJ other = fail $ (show other) ++ " is not supported by annotationToJ"
